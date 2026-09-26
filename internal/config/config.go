8eca1c2ebd277e5b9db5043b8e4873606651193f
alexandro zahreshchanka <alexandro.dev.work@gmail.com>
2026-09-26 13:58:49 +0300

    add tg time

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,7 +22,7 @@ const (
 	defaultBalanceCurrency = "BYN"
 
 	defaultLowBalanceThreshold = 15.0
-	defaultLowBalanceNotifyAt  = "13:02"
+	defaultLowBalanceNotifyAt  = "14:02"
 	defaultNotifyTimezone      = "Europe/Minsk"
 )
 
